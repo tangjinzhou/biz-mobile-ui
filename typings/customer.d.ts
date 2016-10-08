396fb552242f9cc2f057d77aca8511a5f59cb771
tjz <415800467@qq.com>
2016-10-08 13:55:12 +0800

    typings

---
+++ typings/customer.d.ts
@@ -0,0 +1,32 @@
+declare module 'biz-mobile-ui' {
+    var exports: {
+        Alert: any;
+        Button: any;
+        Icon: any;
+        Line: any;
+        Tabs: any;
+        DatePicker: any;
+        InputItem: any;
+        List: any;
+        ListView: any;
+        Progress: any;
+    };
+    export = exports
+}
+declare module 'react-swipeable-views' {
+    var exports: any
+    export default exports
+}
+declare module 'react-swipeable-views/lib/autoPlay' {
+    var exports: any
+    export default exports
+}
+declare module 'react-tap-event-plugin'{
+    var exports:()=>any;
+    export = exports;
+}
+interface BizuiProps {
+    prefixCls?: string,
+    className?: string,
+    style?: {},
+}
